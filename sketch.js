7b718a60274340d42e60d057d1ac43186c5e7d65
iosonosempreio <tommaso.elli@gmail.com>
2019-09-12 15:17:37 +0200

    part of interface

--- sketch.js
+++ sketch.js
@@ -1,11 +1,60 @@
+var cp, sendButton, selectMode, userId;
+
+var transportationModes = ["Machinbombo", "Chapas", "Private Car", "Taxi", "Walk", "Xopela"];
+
+var data = {
+  'id': undefined,
+  'transportationMode': undefined,
+  'positions': []
+}
+
+var positionTemplate = {
+  'longitude': undefined,
+  'latitude': undefined,
+  'timestamp': undefined
+}
+
 function preload(){
   // put preload code here
 }
 
 function setup() {
-  // put setup code here
+  cp = {
+      'lemonchiffon': color(255, 250, 205),
+      'peachpuff': color(255, 218, 185)
+  }
+  createCanvas(windowWidth, windowHeight);
+  background(cp.lemonchiffon);
+
+  var userId = createInput('');
+  userId
+    .position(50, 10)
+    .input(function(){
+      data.id = this.value();
+    });
+
+  selectMode = createSelect();
+  transportationModes.forEach( d => { selectMode.option(d) });
+  selectMode
+    .position(50, 30)
+    .changed(function(){
+      data.transportationMode = this.value();
+    });
+
+  sendButton = createButton('Submit');
+  sendButton
+    .position(50, 100)
+    .style('background-color', cp.peachpuff)
+    .mousePressed(sendData);
 }
 
 function draw() {
   // put drawing code here
+  background(cp.lemonchiffon);
+}
+
+function sendData() {
+
+  console.log('Can geolocate?', geoCheck());
+  console.log('send this data to firebase:', data);
 }
